8efee29a4b6e0b6fa0e47c09a4f28df49f73f02d
Fergus Illman <fergus.illman.au@gmail.com>
2023-08-10 19:28:59 +0930

    changed connect

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Book-SE", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/Book-SE",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 
 module.exports = mongoose.connection;
